f4eac33a5c2873fb5d7217892f7f308d0a76f85c
William Rai <48931640+Williamrai@users.noreply.github.com>
2023-03-25 17:01:05 -0400

    Update simply_halal_api_endpoints.dart

--- lib/network/simply_halal_api_endpoints.dart
+++ lib/network/simply_halal_api_endpoints.dart
@@ -4,6 +4,5 @@ class SimplyHalalApiEndpoints {
   // change the values if different API is chosen
   static const apiURL = "https://api.yelp.com/v3/businesses/search";
   static const businessDetailsUrl = "https://api.yelp.com/v3/businesses";
-  static const apiKey =
-      "MEzunJ1F8Go6J5VJPwyI0-n5m2JFfXgIEajDiXdsCKDNsPOe6HzFnjNXtmnxfPPIRiJgqaZSq67RppZKivWa6ooLHVX2-0uhGHC3QTlK1tvpLIgAevGhLNLX0NgoY3Yx";
+  static const apiKey = "";
 }
